100e86308e78ba9f9999445ed05c88be50084cea
Laurie Fish <iamlauriefish@gmail.com>
2023-04-26 19:59:39 -0700

    tried to fix printing notes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
+// const parse = require('./db/db.json');
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -11,40 +13,48 @@ app.use(express.json());
 
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(_dirname, "/public/index.html"))
+    res.sendFile(path.join(__dirname, "/public/index.html"))
 });
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(_dirname, 'public/notes.html'))
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 });
 
+app.get('api/notes', (req, res) => {
 
+
+});
 
 
 app.post('/api/notes', (req, res) => {
 
     const { title, text } = req.body;
-
+    let newNote;
     if (title && text) {
 
-        let newNote = req.body;
-
-        console.log(newNote);
-        fs.writeFile('db/db.json', JSON.stringify(parsedNotes, null, 4), (Err) =>
-            Err ? console.error(Err) : res.send(newNote)
-        )
-
+        newNote = {
+            title,
+            text,
+            id: uuidv4()
+        }
     };
+    fs.readFile('./db/.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
 
+        } else {
+            const parseData = JSON.parse(data);
+            parseData.push(newNote);
 
+            fs.writeFile('./db/db.json', JSON.stringify(parseData, null, 4), (Err) =>
+                Err ? console.error(Err) :
+                    console.log('test')
+                // res.json(parse)
+            );
+        }
+    }
+    );
 
-
-
-
-
-
-
-
-
-
-
-
+})
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
